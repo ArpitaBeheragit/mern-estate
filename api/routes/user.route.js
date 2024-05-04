64f3a2b1cbfb62a468bee3d47d0aed4a9dbfa90f
ArpitaBeheragit <eee.21beee39@gmail.com>
2024-05-04 13:05:03 +0530

    adding signin and signup

---
+++ api/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utills/verifyUser.js';
+
+const router = express.Router();
+router.get('/test',test)
+router.post('/update/:id',verifyToken, updateUser)
+export default router;
