f9ed1144f031314192fdf2f4dd8315cf3869035b
ArpitaBeheragit <eee.21beee39@gmail.com>
2024-05-04 13:22:37 +0530

    adding signin signup

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,30 @@
+import User from "../models/user.model";
+import { errorHandler } from "../utills/error";
+import bcryptjs from 'bcryptjs'
+
+export const test=(req,res)=>{
+    res.json({
+        message:"Hello World!"
+    })
+}
+
+export const updateUser=async(res, req, next)=>{
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "You are not authenticated")) 
+    try {
+       if(req.body.password){
+        req.body.password=bcryptjs.hashSync(req.body.password, 10)
+       }
+    const updateUser=await User.findByIdAndUpdate(req.params.id,{
+        $set:{
+            username: req.body.username,
+            email:req.body.email,
+            password:req.body.password
+        }
+    },{new:true} )
+    const {password, ...rest}=updateUser._doc
+
+    res.status(200).json(rest)
+    } catch (error) {
+        next(error);
+    }
+}
